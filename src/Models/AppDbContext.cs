8f7579c8d8949ccf377dbd96b802f35f0b93a7c4
Juan Gallardo <juan.gallardo.ab@gmail.com>
2020-01-22 20:05:28 -0700

    Add Identity

--- src/Models/AppDbContext.cs
+++ src/Models/AppDbContext.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AscendedGuild.Models
 {
-	public class AppDbContext : DbContext
+	public class AppDbContext : IdentityDbContext<IdentityUser>
 	{
 		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
 		{			
