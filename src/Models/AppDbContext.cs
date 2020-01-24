0f93e1bf05202ec92b6e4b41f2b0110940777766
Juan Gallardo <juan.gallardo.ab@gmail.com>
2020-01-24 11:17:21 -0700

    Add about-us.html source file

--- src/Models/AppDbContext.cs
+++ src/Models/AppDbContext.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
