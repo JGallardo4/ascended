5c7ba762289f90d08fc13249d275604cdbe12bb9
Juan Gallardo <juan.gallardo.ab@gmail.com>
2020-01-27 22:24:00 -0700

    Add Admin user initialization

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using AscendedGuild.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -50,19 +51,21 @@ namespace AscendedGuild
 			});
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		// This method gets called by the runtime. 
+		// Use this method to configure the HTTP request pipeline.
+		public void Configure(
+			IApplicationBuilder app, IWebHostEnvironment env,
+			IServiceProvider serviceProvider)
 		{
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
 			}
-
+			
+			app.UseAuthentication();	
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
-
 			app.UseRouting();
-			app.UseAuthentication();
 			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
@@ -73,6 +76,51 @@ namespace AscendedGuild
 				
 				endpoints.MapRazorPages();
 			});
+
+			CreateRoles(serviceProvider).Wait();
+		}
+
+		// This method ensures there is one and only one admin account at all times
+		private async Task CreateRoles(IServiceProvider serviceProvider)
+		{
+			// Initialize custom roles
+			var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+			var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+			string[] roles = {"Administrator"};
+
+			foreach (var roleName in roles)
+			{
+				var roleExists = 
+					await roleManager.RoleExistsAsync(roleName);
+				
+				if (!roleExists)
+				{
+					await roleManager.CreateAsync(new IdentityRole(roleName));
+				}
+			}
+
+			// Find the user with the admin email
+			var adminEmail = Configuration.GetSection("UserSettings")["UserEmail"];
+      var _user = await userManager.FindByEmailAsync(adminEmail);
+
+			if (_user == null)
+			{
+				// Create admin user
+				var adminUser = new IdentityUser()
+				{
+					UserName = adminEmail,
+					Email = adminEmail
+				};
+				string adminPassword = Configuration.GetSection("UserSettings")["UserPassword"];
+
+				var createAdmin = await userManager.CreateAsync(adminUser, adminPassword);
+
+				if (createAdmin.Succeeded)
+				{
+					// Assign admin user to admin role
+					await userManager.AddToRoleAsync(adminUser, "Administrator");
+				}
+			}
 		}
 	}
 }
